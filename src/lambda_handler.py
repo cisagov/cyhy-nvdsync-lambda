39adef4333001a296656bba5eee6a794b07e1bc3
Michael Saki <michael.saki@associates.cisa.dhs.gov>
2023-08-24 14:56:07 -0500

    Add concurent CVE URL processing
    After receiving feedback from the
    PR, I made the lambda run multiple
    CVE URLS concurently. However, this
    required me to change the way we were
    reading the CVE data. Now we read
    using aiohttp which is async by design.

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -2,7 +2,6 @@
 
 # Standard Python Libraries
 import asyncio
-from asyncio import Semaphore
 from datetime import datetime
 import gzip
 from io import BytesIO
@@ -10,10 +9,9 @@ import json
 import logging
 import os
 from typing import Optional
-import urllib.parse
-import urllib.request
 
 # Third-Party Libraries
+import aiohttp
 from beanie import Document, init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -64,18 +62,29 @@ class CVEDoc(Document):
                 self.severity = 2
             else:
                 self.severity = 1
+
         await super().save(*args, **kwargs)
 
 
 async def process_nvd(cve) -> str:
     """Add the provided CVE to the database and return its id."""
     # Fill fields for the CVE Documents from the given JSON files
-    cve_id = cve["cve"]["CVE_data_meta"]["ID"]
+    try:
+        cve_id = cve["cve"]["CVE_data_meta"]["ID"]
+    except KeyError:
+        # JSON might be malformed, so we'll log what the CVE object looks like
+        # and then raise an error
+        logger.error("CVE object: %s", cve)
+        raise ValueError("JSON does not look like valid NVD CVE data.")
+    # All fields are there but 'ID' field is empty
+    if not cve_id:
+        raise ValueError("CVE ID is empty.")
+
     # Reject or remove CVEs that don't have baseMetricV2 or baseMetricV3 CVSS data
     if not any(k in cve["impact"] for k in ["baseMetricV2", "baseMetricV3"]):
         """If the CVE is in the database, it needs to be deleted from the database"""
 
-        if CVEDoc.find_one(CVEDoc.id == cve_id):
+        if await CVEDoc.find_one(CVEDoc.id == cve_id):
             print("x", end="")
             remove_cve_doc = CVEDoc(id=cve_id)
             await remove_cve_doc.delete()
@@ -95,23 +104,22 @@ async def process_nvd(cve) -> str:
         cvss_version_temp = cve["impact"]["baseMetric" + version]["cvss" + version][
             "version"
         ]
-
         # Fill document fields with CVE data
         print(".", end="")
-        
-        print("Adding CVE: ", cve_id, " to database")
 
-        cve_doc = CVEDoc(
-            id=cve_id, cvss_score=float(cvss_base_score), cvss_version=cvss_version_temp
-        )
+        try:
+            cve_doc = CVEDoc(
+                id=cve_id,
+                cvss_score=float(cvss_base_score),
+                cvss_version=cvss_version_temp,
+                severity=None,
+            )
+        except ValueError:
+            logger.error("CVE object: %s", cve)
+            raise ValueError("JSON does not look like valid NVD CVE data.")
 
-        if not cve_id:
-            raise ValueError("JSON does not look like valid CISA CVE data.")
+        await cve_doc.save()
 
-        
-        # Gather all tasks to run concurrently
-        await asyncio.gather(*[asyncio.create_task(cve_doc.save())])
-        
         return cve_doc.id
 
 
@@ -123,54 +131,61 @@ async def process_cve_json(json_stream) -> None:
         raise ValueError("JSON does not look like valid NVD CVE data.")
 
     tasks = [asyncio.create_task(process_nvd(cve)) for cve in data.get("CVE_Items", [])]
+
     await asyncio.gather(*tasks)
-    
-    
-async def process_file(socket) -> None:
-    """With the open socket, unzip the file and process it with process_cve_json()."""
-    # Open the file and read it into memory
-    buf = BytesIO(socket.read())
-    f = gzip.GzipFile(fileobj=buf)
-    await process_cve_json(f)
+
+    # Show the time at which the JSON was processed
+    print("Finished processing JSON at ", datetime.utcnow().isoformat())
+
+
+async def unzip_and_process(content):
+    """Unzip content and process it with process_cve_json()."""
+    with gzip.GzipFile(fileobj=BytesIO(content)) as f:
+        await process_cve_json(f)
+
+
+async def fetch_url_content(session, url):
+    """Fetch the content of the provided URL."""
+    async with session.get(url) as response:
+        return await response.read()
 
 
 async def process_urls(cve_urls, db) -> None:
     """Initialize beanie ODM and begin processing CVE URLs."""
     await init_beanie(database=motor_client[db], document_models=[CVEDoc])
-    # Async for loop that processes each of the CVE URLs individually
-    
-    for cve_url in cve_urls:
-        # We disable the bandit blacklist for the urllib.request.urlopen() function
-        # because the URL is either the defaul (safe) URL or one provided in the
-        # Lambda configuration so we can assume it is safe.
-        # Try to connect to the socket and if it fails, try 10 more times before
-        # giving up.
-        attempts = 0
-        retries = 10
-        
-        while True:
-            try:
-                print("Before socket")
-                socket = urllib.request.urlopen(cve_url)  # nosec B310
-                print("After socket")
-                break
-            except (urllib.error.HTTPError, urllib.error.URLError) as err:
-                logging.debug(
-                    "Encountered a(n) %s exception while attempting to open socket from '%s'",
-                    type(err).__name__,
-                    cve_url,
-                )
-                attempts += 1
-                await asyncio.sleep(5)
-                if attempts <= retries:
-                    logging.warning(
-                        "Performing retry %d/%d for '%s'", attempts, retries, cve_url
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+
+        for cve_url in cve_urls:
+            attempts = 0
+            retries = 10
+
+            while True:
+                try:
+                    print("Before socket")
+                    content = await fetch_url_content(session, cve_url)
+                    tasks.append(asyncio.create_task(unzip_and_process(content)))
+                    break
+                except aiohttp.ClientError as err:
+                    logging.debug(
+                        "Encountered a(n) %s exception while attempting to fetch URL '%s'",
+                        type(err).__name__,
+                        cve_url,
                     )
-                else:
-                    raise err
-        
-        await process_file(socket)    
-        socket.close()
+                    attempts += 1
+                    await asyncio.sleep(5)
+                    if attempts <= retries:
+                        logging.warning(
+                            "Performing retry %d/%d for '%s'",
+                            attempts,
+                            retries,
+                            cve_url,
+                        )
+                    else:
+                        raise err
+    await asyncio.gather(*tasks)
+
 
 def generate_urls():
     """Return the NVD URLs for each year."""
@@ -229,7 +244,7 @@ def handler(event, context) -> None:
         cve_json_urls = nvd_urls
     else:
         cve_json_urls = [json_url]
-    
+
     # Determine if a non-default collection is being used
     db_collection = os.environ.get("target_collection")
     if db_collection is not None:
